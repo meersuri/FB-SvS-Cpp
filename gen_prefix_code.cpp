4a18758753b598dee6caa971db224e56900a8aa4
meersuri <meersuri@gmail.com>
2020-08-15 21:57:59 +0530

    add code tree traversal to read codewords

--- gen_prefix_code.cpp
+++ gen_prefix_code.cpp
@@ -35,9 +35,10 @@ std::discrete_distribution<int> Node::distribution = std::discrete_distribution<
 
 void grow_tree(Node* node, size_t depth, size_t max_depth)
 {
+	//std::cout << node->val << " " << depth << std::endl;
 	if(node == nullptr)
 		return;
-	if(depth == max_depth + 1)
+	if(depth == max_depth)
 		return;
 	node->extend();
 	for(int i = 0; i < node->children.size(); ++i)
@@ -47,11 +48,36 @@ void grow_tree(Node* node, size_t depth, size_t max_depth)
 	}
 	return;
 }	
+void traverse_tree(Node* node, std::string& code, std::vector<std::string>& codewords)
+{
+	if(node == nullptr)
+		return;
+	code.push_back(node->val);
+	bool is_leaf = true;
+	for(int i = 0; i < node->children.size(); ++i)
+	{
+		if(node->children[i] != nullptr)
+		{
+			is_leaf = false;
+			traverse_tree(node->children[i], code, codewords);
+		}
+	}
+	if(is_leaf)
+		codewords.push_back(code.substr(1, code.size() - 1));
+	code.pop_back();
+	return;
+}
 		
 int main()
 {
 	size_t max_depth = 5;
 	Node* root = new Node('X');
 	grow_tree(root, 0, max_depth);
+	std::string code;
+	std::vector<std::string> codewords;
+	traverse_tree(root, code, codewords);
+        for(int i =0; i < codewords.size(); ++i)
+		std::cout << codewords[i] << std::endl;
+	std::cout << std::endl;	
 	return 0;
 }
