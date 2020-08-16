776f6d1f43d1535cc1646d9f372954225b67320e
meersuri <meersuri@gmail.com>
2020-08-16 17:23:21 +0530

    fixed previous commit

--- gen_prefix_code.cpp
+++ gen_prefix_code.cpp
@@ -22,17 +22,21 @@ Node::Node(char node_val): val(node_val)
 
 void Node::extend()
 {
-	for(int i = 0; i < Node::alphabet.size(); ++i)
+	bool to_extend = Node::distribution(Node::generator);
+	if(to_extend)
 	{
-		bool to_extend = Node::distribution(Node::generator);
-		if(to_extend)
-			children[i] = new Node(Node::alphabet[i]);
+
+		for(int i = 0; i < Node::alphabet.size(); ++i)
+		{
+			if(to_extend)
+				children[i] = new Node(Node::alphabet[i]);
+		}
 	}
 }
 
 std::default_random_engine Node::generator;
-//std::vector<char> Node::alphabet = std::vector<char>{'A', 'C', 'G', 'T', 'N'};
-std::vector<char> Node::alphabet = std::vector<char>{'U', 'T', 'A', 'W', 'C', 'Y', 'M', 'H', 'G', 'K', 'R', 'D', 'S', 'B', 'V', 'N'};
+std::vector<char> Node::alphabet = std::vector<char>{'A', 'C', 'G', 'T', 'N'};
+//std::vector<char> Node::alphabet = std::vector<char>{'U', 'T', 'A', 'W', 'C', 'Y', 'M', 'H', 'G', 'K', 'R', 'D', 'S', 'B', 'V', 'N'};
 std::discrete_distribution<int> Node::distribution = std::discrete_distribution<int>{0.55, 0.45};
 
 void grow_tree(Node* node, size_t depth, size_t max_depth)
@@ -74,7 +78,11 @@ void traverse_tree(Node* node, std::string& code, std::vector<std::string>& code
 void gen_prefix_code(size_t max_code_len, std::vector<std::string>& codebook)
 {
 	Node* root = new Node('X');
-	grow_tree(root, 0, max_code_len);
+	for(int i =0; i < root->alphabet.size(); ++i)
+	{
+		root->children[i] = new Node(root->alphabet[i]);
+		grow_tree(root->children[i], 0, max_code_len);
+	}
 	std::string code;
 	traverse_tree(root, code, codebook);
 	return;
@@ -86,7 +94,7 @@ int main()
 	std::vector<std::string> codebook;
 	gen_prefix_code(max_depth, codebook);
 	std::cout << "codebook size = " << codebook.size() << std::endl;
-	std::ofstream ofs("codebook.txt");
+	std::ofstream ofs("codebook_dna_1.txt");
 	for(int i =0; i < codebook.size(); ++i)
 		ofs << codebook[i] << std::endl;
 //        for(int i =0; i < codewords.size(); ++i)
