19b6fed539af61752d216d5786ea85fdd27fadba
meersuri <meersuri@gmail.com>
2020-08-22 16:04:58 +0530

    added cmd line params

--- gen_k_grams.cpp
+++ gen_k_grams.cpp
@@ -5,19 +5,24 @@
 #include<fstream>
 #include"utils.h"
 
-int main()
+int main(int argc, char** argv)
 {
-	size_t k = 8;
-	std::vector<char> alphabet = {'A', 'T', 'C', 'G'};
+	if(argc < 4)
+	{
+	std::cerr << "usage - " << argv[0] << "  " << "alphabet" << "  " << "codeword length" << "  " << "output file" << std::endl;
+		return -1;
+	}
+	std::vector<char> alphabet;
+	std::string alphabet_string = argv[1];
+	for(int i = 0; i < alphabet_string.size(); ++i)
+		alphabet.push_back(alphabet_string[i]);
+	size_t k = std::stoi(argv[2]);
+	std::string out_file = argv[3];
+
 	std::vector<std::string> codebook;
 	gen_k_grams_code(k, alphabet, codebook);
-//	std::cout << "k-gram code words - " << std::endl;
-//	for(int i = 0; i < code.size(); ++i)
-//		std::cout << code[i] << std::endl;
 	std::cout << "codebook size = " << codebook.size() << std::endl;
-	std::ofstream ofs("k_grams_dna_" + std::to_string(k) + ".txt");
-	for(int i =0; i < codebook.size(); ++i)
-		ofs << codebook[i] << std::endl;
+	write_codebook(out_file, codebook, alphabet);
 	return 0;
 }
 
