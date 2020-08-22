bd20eb8b007b820f94acc5c4a0e998caebff7413
meersuri <meersuri@gmail.com>
2020-08-22 15:20:20 +0530

    added cmd line params to build and write index to disk

--- build_index_codebook.cpp
+++ build_index_codebook.cpp
@@ -7,40 +7,27 @@
 #include<fstream>
 #include"utils.h"
 
-int main()
+int main(int argc, char** argv)
 {
-	std::vector<char> alphabet = {'A', 'T', 'C', 'G'}; 
-	size_t alphabet_size = alphabet.size();
-	std::ifstream ifs("k_grams_dna_8.txt");
+	if(argc < 4)
+	{
+		std::cerr << "usage - " << argv[0] << "\tpath to codebook" << "\tpath to sequence to index" << "\toutput file" << std::endl; 
+		return -1;
+	}
+	std::string codebook_file = argv[1];
+	std::string ref_seq_file = argv[2];
+	std::string out_file = argv[3];
+	
+	std::vector<char> alphabet; 
 	std::vector<std::string> codebook;
-	char buffer[100];
-	while(ifs.getline(buffer, 100))
-		codebook.push_back(std::string(buffer));
-	auto max_code_it = std::max_element(codebook.begin(), codebook.end(), [] (std::string x1, std::string x2){ return x1.size() < x2.size();});
-	size_t max_code_len = max_code_it->size();
-
-	ifs = std::ifstream("/mnt/d/ecoli.fasta");
 	std::string ref_seq;
-	while(ifs.getline(buffer, 100))
-	{
-		if(buffer[0] != '>')
-			ref_seq += std::string(buffer);
-	}
-	std::cout << "reference sequence length = " << ref_seq.size() << std::endl;
 	std::unordered_map<std::string, std::vector<size_t>> index;
+
+	load_codebook(codebook_file, codebook, alphabet);
+	load_ref_seq(ref_seq_file, ref_seq);
+	std::cout << "reference sequence length = " << ref_seq.size() << std::endl;
 	build_index(ref_seq, codebook, index);
-	std::ofstream ofs("ecoli_k_grams_dna_8_index.txt");
-	ofs << "index v1" << std::endl;
-	for(auto it = index.begin(); it != index.end(); ++it)
-	{
-		ofs << it->first << std::endl;
-		size_t posting_list_len = it->second.size();
-		ofs << std::to_string(posting_list_len) << std::endl;
-		for(int i = 0; i < posting_list_len; ++i)
-			ofs << std::to_string(it->second[i]) << " ";
-		ofs << std::endl;
-	}
-	ofs.close();
+	write_index(out_file, index);
 	return 0;
 }
 	       	
