f7e9f223192a37fb9987a0afb49bd179da7448e7
meersuri <meersuri@gmail.com>
2020-08-23 12:02:24 +0530

    added cmd line params and first order source model

--- dna_test.cpp
+++ dna_test.cpp
@@ -3,40 +3,60 @@
 #include<fstream>
 #include<random>
 #include<omp.h>
+#include<unordered_map>
 #include"utils.h"
 
-int main()
+void calc_symbol_probs(std::string& ref_seq, std::vector<char>& alphabet, std::vector<float>& symbol_probs)
 {
-	std::ifstream ifs("codebook_dna_3.txt");
-	std::vector<std::string> codebook;
-	char buffer[100];
-	while(ifs.getline(buffer, 100))
-		codebook.push_back(std::string(buffer));
+	std::unordered_map<char, int> symbol_to_index;
+	for(int i = 0; i < alphabet.size(); ++i)
+		symbol_to_index.insert(std::make_pair(alphabet[i], i));
 
-	auto max_code_it = std::max_element(codebook.begin(), codebook.end(), [] (std::string x1, std::string x2){ return x1.size() > x2.size();});
-	size_t max_code_len = max_code_it->size();
-	ifs = std::ifstream("/mnt/d/ecoli.fasta");
-	std::string ref_seq;
-	while(ifs.getline(buffer, 100))
+	for(int i = 0; i < ref_seq.size(); ++i)
+		++symbol_probs[symbol_to_index[ref_seq[i]]];
+
+	for(int i = 0; i < alphabet.size(); ++i)
+		symbol_probs[symbol_to_index[ref_seq[i]]] /= ref_seq.size();
+}
+
+
+int main(int argc, char** argv)
+{
+	if(argc < 6)
 	{
-		if(buffer[0] != '>')
-			ref_seq += std::string(buffer);
+		std::cerr << "usage - " << argv[0] << "  " << "path to codebook" <<  "  " <<
+		       	"path to ref seq"<< "  " << "number of queries" << 
+		       "  " << 	"min query len" << "  " << "max query len" << std::endl; 
+		return -1;
 	}
-	std::cout << "reference sequence length = " << ref_seq.size() << std::endl;
-	std::cout << ref_seq.substr(0, 140) << std::endl;
-	const std::vector<char> alphabet{'A', 'C', 'G', 'T'}; 
-	size_t alphabet_size = alphabet.size();
-	std::default_random_engine generator;
-	std::uniform_int_distribution<int> source_dist(0, alphabet_size - 1);
+	std::string codebook_file = argv[1];
+	std::string ref_seq_file = argv[2];
+	size_t n_queries = std::stoi(argv[3]);
+	size_t min_query_len = std::stoi(argv[4]);
+	size_t max_query_len = std::stoi(argv[5]);
+	
+	std::vector<char> alphabet; 
+	std::string ref_seq;
+	std::vector<std::string> codebook;
 	std::unordered_map<std::string, std::vector<size_t>> index;
+	std::vector<std::string> queries;
+
+	load_codebook(codebook_file, codebook, alphabet);
+	std::cout <<"finished loading codebook" << std::endl;
+	auto max_code_it = std::max_element(codebook.begin(), codebook.end(), [] (std::string x1, std::string x2){ return x1.size() < x2.size();});
+	size_t max_code_len = max_code_it->size();
+	load_ref_seq(ref_seq_file, ref_seq);
+	std::cout <<"finished loading ref sequence" << std::endl;
+
+	size_t alphabet_size = alphabet.size();
+	std::vector<float> symbol_probs(alphabet.size(), 0);
+	calc_symbol_probs(ref_seq, alphabet, symbol_probs);
+	std::default_random_engine generator;
+	std::discrete_distribution<int> source_dist(symbol_probs.begin(), symbol_probs.end());
 	build_index(ref_seq, codebook, index);
 	std::cout <<"finished building index" << std::endl;
-//	std::string query = ref_seq.substr(3141, query_len);
-	size_t max_query_len = 1000;
-	size_t min_query_len = 20;
-	size_t n_queries = 1000;
 	std::uniform_int_distribution<int> query_len_dist(min_query_len, max_query_len);
-	std::vector<std::string> failed_tests;
+	std::vector<std::pair<std::string, size_t>> failed_tests;
 	#pragma omp parallel for
 	for(int i = 0; i < n_queries; ++i)
 	{
@@ -44,7 +64,7 @@ int main()
 		std::string query(query_len, alphabet[0]);
 		for(int j = 0; j < query_len; ++j)
 			query[j] = alphabet[source_dist(generator)];
-		if((i % 100) == 0)
+		if((i % (int)(n_queries/10)) == 0)
 			std::cout << "testing query " << i + 1 << " " << query << std::endl;
 		std::vector<size_t> fb_svs_match_pos;
 		int n_matches = fb_svs(query, index, max_code_len, fb_svs_match_pos);
@@ -62,14 +82,13 @@ int main()
 				break;
 		}
 		if(fb_svs_match_pos != str_find_match_pos)
-		{
 //			std::cout << "failed test case - " << query << std::endl;
-			failed_tests.push_back(query);
-		}
+			failed_tests.push_back(std::make_pair(query, i));
 	}
 	std::cout << failed_tests.size() << " failed test cases" << std::endl;
 	for(int i = 0; i < failed_tests.size(); ++i)
-		std::cout << failed_tests[i] << std::endl;
+		std::cout << failed_tests[i].second << " ";
+	std::cout << std::endl;
 	return 0;
 }
 
